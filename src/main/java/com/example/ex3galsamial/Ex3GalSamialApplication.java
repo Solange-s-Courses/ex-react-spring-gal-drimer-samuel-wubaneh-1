5650caef66f8074052c97f997e30091f68ea2823
gal drimer <galdr@edu.hac.ac.il>
2025-05-15 18:27:04 +0300

    try pushing

---
+++ src/main/java/com/example/ex3galsamial/Ex3GalSamialApplication.java
@@ -0,0 +1,13 @@
+package com.example.ex3galsamial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex3GalSamialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex3GalSamialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ex3galsamial/Ex3GalSamialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ex3galsamial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex3GalSamialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
