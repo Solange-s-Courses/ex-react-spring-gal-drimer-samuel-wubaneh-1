f5ac8eab1ef8cf747b9c1650c47bc09366535eae
gal drimer <galdr@edu.hac.ac.il>
2025-05-26 16:17:09 +0300

    push

---
+++ src/main/java/com/example/exreactspringgaldrimersamuelwubaneh1/ExReactSpringGalDrimerSamuelWubaneh1Application.java
@@ -0,0 +1,13 @@
+package com.example.exreactspringgaldrimersamuelwubaneh1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExReactSpringGalDrimerSamuelWubaneh1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExReactSpringGalDrimerSamuelWubaneh1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/exreactspringgaldrimersamuelwubaneh1/ExReactSpringGalDrimerSamuelWubaneh1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exreactspringgaldrimersamuelwubaneh1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExReactSpringGalDrimerSamuelWubaneh1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
